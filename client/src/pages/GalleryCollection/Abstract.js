c8ebb268ad864f31a45136326a1e7a38183421ba
daze_77 <daze77@gmail.com>
2021-06-11 11:40:49 -0400

    added the Abstract page with image and JSON update to Gallery Collection page

--- client/src/pages/GalleryCollection/Abstract.js
+++ client/src/pages/GalleryCollection/Abstract.js
@@ -1,26 +1,52 @@
 import React from 'react'
 
+import AbstractCard from '../../components/GalleryCard/GalleryCard'
+import GCJSON  from '../../utils/GalleryColGrid.json'
+
 
 function Abstract() {
 
 
-
+ const URL = 'https://www.tonykoukos.com/media/catalog/product/cache/1/thumbnail/314x299/9df78eab33525d08d6e5fb8d27136e95/g/a/'
 
 
 
 return(
+
+   
+
     <>
+    <div class="container">
+        <section >
+            <h1>Abstract</h1>
+            <hr />
 
-        <div class="container">
-            <section >
-                <h1>Abstract</h1>
-                <hr />
 
-            </section>   
-        </div>
+            <div class="row row-cols-1 row-cols-md-3 g-3 wtpCollection">
+                {GCJSON.map( gcA =>( gcA.SubLink.map(gcAS        => (
 
-         
+                    <AbstractCard 
 
+                    Id = {gcAS.Id}
+                    Title = {gcAS.Title}
+                    ImageName = {URL + gcAS.Image}
+
+
+
+                    />
+
+                
+                    ))
+
+
+                ))}
+
+            </div>
+
+        </section>   
+    </div>
+  
+  
     </>
 
 
