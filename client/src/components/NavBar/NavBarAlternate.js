e122c9329f8df1ea76645859a4ed99137c315ba6
daze_77 <daze77@gmail.com>
2021-06-17 23:26:41 -0400

    added some additional formatting to the submenu

--- client/src/components/NavBar/NavBarAlternate.js
+++ client/src/components/NavBar/NavBarAlternate.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react' 
+import { NavLink } from 'react-router-dom'
 
 import NavBarLogo from '../../components/Logo/Logo'
 import NavLogin from './NavLogin'
@@ -6,17 +7,24 @@ import NavLogin from './NavLogin'
 function NavBarAlt(){
 
     const [menuDropdown, setMenuDropdown] = useState("0px")
+    const [subMenu1, setSubMenu1] = useState("")
+    const [subMenu2, setSubMenu2] = useState("")
+
+    const [subMenu3, setSubMenu3] = useState("")
+
     
 
 
-    function handleMenuDropdown(){
+    function handleMenuDropdown(){ 
+        console.log(`menu button pushed`)     
+        menuDropdown==="500px"? setMenuDropdown('0px'): setMenuDropdown('500px')
+    }
 
-        console.log(`menu button pushed`)
-        
-        menuDropdown==="100px"? setMenuDropdown('0px'): setMenuDropdown('100px')
-        
-      
-
+    function handleSubMenu(e){
+        console.log(`submenu activated`, e)
+        subMenu1==="none"? setSubMenu1("block"):setSubMenu1("none")
+        subMenu2==="none"? setSubMenu2("block"):setSubMenu2("none")
+        subMenu3==="none"? setSubMenu3("block"):setSubMenu3("none")
 
     }
 
@@ -30,7 +38,13 @@ function NavBarAlt(){
                     </div>
                     <div class="col menuItems">
                         <ul class="menuList ">
-                            <li onClick={handleMenuDropdown}>PRODUCTS</li>
+                            <li onClick={() =>
+                                {
+                                    handleMenuDropdown();
+                                    handleSubMenu();
+                                
+                                }}
+                                >PRODUCTS</li>
                             <li onClick={handleMenuDropdown}>SERVICES</li>
                             <li onClick={handleMenuDropdown}>THE ARTIST</li>
                         </ul>
@@ -41,6 +55,24 @@ function NavBarAlt(){
                 </div>
                 <div class="row navDropDownRow">
                     <div class="col navDropDowns" style={{height: menuDropdown}}>
+                        <ul class="sub-menu" style={{display: subMenu1}}>
+                            <NavLink to="/GalleryCollection"><li class="">Gallery Collection</li></NavLink>
+                            <NavLink to="/WallCoverings"><li class="">Wall Coverings</li></NavLink>
+                            <NavLink to="/WorldTravelPhotography"><li class="">World Travel Photography</li></NavLink>
+                            <NavLink to="/ArtandDesign"><li class="">Art & Design</li></NavLink>
+                        </ul>
+                        <ul class="sub-menu"  style={{display: subMenu2}}>
+                            <NavLink to="/HospitalityandDesign"><li class="">Hospitality & Design</li></NavLink>
+                            <NavLink to="/Installations"><li class="m">Installations</li></NavLink>
+                            <NavLink to="/Lighting"><li class="">Lighting</li></NavLink>
+                            <NavLink to="/TheFrameShop"><li class="">The Frame Shop</li></NavLink>
+                            <NavLink to="/PrintingandServices"><li class="">Printing & Services</li></NavLink>
+                        </ul>
+                        <ul class="sub-menu" style={{display: subMenu3}}>
+                            <NavLink to="/TheArtist"><li class="">The Artist</li></NavLink>
+                            <NavLink to="/Testimonials"><li class="">Testimonials</li></NavLink>
+                            <NavLink to="/Contact"><li class="">Contact</li></NavLink>
+                        </ul>
 
                     </div>
 
