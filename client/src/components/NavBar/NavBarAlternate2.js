74a54ca978d1c223a893e688d79340237bb532a6
daze_77 <daze77@gmail.com>
2021-06-21 19:40:06 -0400

    added a plus and minus sign to expand the sub menu

--- client/src/components/NavBar/NavBarAlternate2.js
+++ client/src/components/NavBar/NavBarAlternate2.js
@@ -9,12 +9,22 @@ let count =0
 
 function NavBarAlt(){
 
+    let minus = String.fromCharCode(45)
+    const plus = String.fromCharCode(43)
+
+    console.log(plus)
+
     const [menuDropdown, setMenuDropdown] = useState("0px")
-    const [subMenu1, setSubMenu1] = useState("block")
-    const [subMenu2, setSubMenu2] = useState("block")
-    const [subMenu3, setSubMenu3] = useState("block")
+    const [subMenu1, setSubMenu1] = useState("none")
+    const [subMenu2, setSubMenu2] = useState("none")
+    const [subMenu3, setSubMenu3] = useState("none")
     const [menuSelect, setMenuSelect] = useState('')
     const [navOveraly, setNavOverlay] = useState("0px")
+    const [subMenuExpand1, setsubMenuExpand1] = useState (plus)
+    const [subMenuExpand2, setsubMenuExpand2] = useState (plus)
+
+    const [subMenuExpand3, setsubMenuExpand3] = useState (plus)
+
 
     function handleNavOverlay(){
         console.log(`symbol clicked`)
@@ -26,23 +36,35 @@ function NavBarAlt(){
     }
 
     function handleSubMenu(e){
-        console.log(e.target.firstChild.data
-            )
-
-        switch(e.target.firstChild.data){
+        console.log(`this is e`, e)
+        switch(e.target.id){
             case "PRODUCTS":
-                subMenu1 === "block"? setSubMenu1("none") : setSubMenu1("block");
+                subMenu1 === "block" ? setSubMenu1("none")  : setSubMenu1("block")
                 break;
             case "SERVICES":
-                subMenu2 === "block"? setSubMenu2("none") : setSubMenu2("block");
+                subMenu2 === "block" ? setSubMenu2("none") : setSubMenu2("block");
                 break;
             default:
-                subMenu3 === "block"? setSubMenu3("none") : setSubMenu3("block");
+                subMenu3 === "block" ? setSubMenu3("none") : setSubMenu3("block");
+
+        }
+        test(e)
+     }
+
+     function test(e){
+        console.log(`this is e`, e)
+        switch(e.target.id){
+            case "PRODUCTS":
+                subMenu1 === "block" ? setsubMenuExpand1(plus) : setsubMenuExpand1(minus);
+                break;
+            case "SERVICES":
+                subMenu2 === "block" ? setsubMenuExpand2(plus) : setsubMenuExpand2(minus);
+                break;
+            default:
+                subMenu3 === "block" ? setsubMenuExpand3(plus) : setsubMenuExpand3(minus);
 
         }
      }
-
-
     
 
 
@@ -70,45 +92,43 @@ function NavBarAlt(){
                     </div>
                 </div>
                 <div class="navOverlay" style={{height: navOveraly}}>
-                    <div class="navOverlayContent" >
+                    <div class="navOverlayHeader" >
                         <ul class="navOverlaySubMenu">
                             <li id="closeButton" onClick={handleCloseButton}>&times;</li>
                             <li id="subMenuLogo"><img  src={LogoSymbol} alt="Logo"/>   </li>
                         </ul>
                     </div>
                     <div class="fullMenuList ">
-                        <ul >
-                            <li onClick={handleSubMenu}>PRODUCTS
-                                <ul class="sub-menuList" style={{display: subMenu1}}>
+                        <div  id="PRODUCTS" onClick={handleSubMenu}>PRODUCTS &#32; {subMenuExpand1}
+                            <ul class="sub-menuList" style={{display: subMenu1}}>
                                     <NavLink to="/GalleryCollection"><li class="">Gallery Collection</li></NavLink>
                                     <NavLink to="/WallCoverings"><li class="">Wall Coverings</li></NavLink>
                                     <NavLink to="/WorldTravelPhotography"><li class="">World Travel Photography</li></NavLink>
                                     <NavLink to="/ArtandDesign"><li class="">Art & Design</li></NavLink>
-                                </ul>
-                            
-                            </li>
+                            </ul>
+                        </div>
+                        <div  id="SERVICES" onClick={handleSubMenu}>SERVICES &#32; {subMenuExpand2}
+                            <ul class="sub-menuList"  style={{display: subMenu2}}>
+                                <NavLink to="/HospitalityandDesign"><li class="">Hospitality & Design</li></NavLink>
+                                <NavLink to="/Installations"><li class="m">Installations</li></NavLink>
+                                <NavLink to="/Lighting"><li class="">Lighting</li></NavLink>
+                                <NavLink to="/TheFrameShop"><li class="">The Frame Shop</li></NavLink>
+                                <NavLink to="/PrintingandServices"><li class="">Printing & Services</li></NavLink>
+                            </ul>
+                        </div>
+                        <div  id="THEARTIST" onClick={handleSubMenu}>THE ARTIST &#32; {subMenuExpand3}
+                            <ul class="sub-menuList" style={{display: subMenu3}}>
+                                <NavLink to="/TheArtist"><li class="">The Artist</li></NavLink>
+                                <NavLink to="/Testimonials"><li class="">Testimonials</li></NavLink>
+                                <NavLink to="/Contact"><li class="">Contact</li></NavLink>
+                            </ul>
+                    
+                        </div>
+                    </div>                
 
-
-
-                            <li onClick={handleSubMenu}>SERVICES
-                                <ul class="sub-menuList"  style={{display: subMenu2}}>
-                                    <NavLink to="/HospitalityandDesign"><li class="">Hospitality & Design</li></NavLink>
-                                    <NavLink to="/Installations"><li class="m">Installations</li></NavLink>
-                                    <NavLink to="/Lighting"><li class="">Lighting</li></NavLink>
-                                    <NavLink to="/TheFrameShop"><li class="">The Frame Shop</li></NavLink>
-                                    <NavLink to="/PrintingandServices"><li class="">Printing & Services</li></NavLink>
-                                </ul>
-                            </li>
-                            <li onClick={handleSubMenu}>THE ARTIST
-                                <ul class="sub-menuList" style={{display: subMenu3}}>
-                                    <NavLink to="/TheArtist"><li class="">The Artist</li></NavLink>
-                                    <NavLink to="/Testimonials"><li class="">Testimonials</li></NavLink>
-                                    <NavLink to="/Contact"><li class="">Contact</li></NavLink>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
+
+
 
             </nav>
 
