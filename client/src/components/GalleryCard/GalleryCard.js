707005ad880bf0a6cf5b8da5910549c43fd2939e
daze_77 <daze77@gmail.com>
2021-10-08 17:38:54 -0400

    cleaned up some test code from WTP page

--- client/src/components/GalleryCard/GalleryCard.js
+++ client/src/components/GalleryCard/GalleryCard.js
@@ -25,7 +25,7 @@ function GalleryCard (props) {
                     }}>
                     <div  ref={imgRef}  className="card gallerygrid"  > 
                         
-                         <img className="card-img test" src={isInView ? props.ImageName : ""} alt={props.ImageName}/>
+                         <img className="card-img" src={isInView ? props.ImageName : ""} alt={props.ImageName}/>
         
                         <div className="card-img-overlay">
                             <h5 className="card-title w-100 ">{Title}</h5>
