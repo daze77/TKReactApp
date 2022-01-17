7666dc0816ed670838b1bff0e24c8f4c1f562ff4
daze_77 <daze77@gmail.com>
2022-01-17 17:13:45 -0500

    added a count to basket and changed color to blue if count greater than 0

--- client/src/components/BasketBtn/BasketBtn.js
+++ client/src/components/BasketBtn/BasketBtn.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './BasketBtn.css'
+import { useStoreContext } from '../../utils/GlobalStore'
+
 
 
 function BasktBtn(){
+    const [{ ...data } , ]= useStoreContext()
 
-
+    const items = data.basketList[1].basket.length
 
     return(
         <>
 
-            <Link to='/CheckoutPage'><i class="fas fa-shopping-bag"></i></Link>
+            <Link to='/CheckoutPage'>
+                <i class={`fas fa-shopping-bag ${items>0? `shopingBagwItems`:""}`}>
+                    {items>0 &&<div class="basktItems ">
+                        {items}
+                    </div>}
+                </i>
+            </Link>
 
         </>
     )
