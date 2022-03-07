b2f7a0b5fdf3190f7c7dda5f620847fd0cbfe3ed
daze_77 <daze77@gmail.com>
2022-03-07 15:14:19 -0500

    updated formatting and css of subscriptions in the footer

--- client/src/components/Footer/Subscriptions.js
+++ client/src/components/Footer/Subscriptions.js
@@ -33,64 +33,59 @@ function Subscriptions(){
 
     return(
         <>
-            <form className="row g-3 subscriptions mb-3" onSubmit={subscriptionSubmit} >
-                <div className=" input-group col-md-3 ">
-                    <div className="form-floating " >
-                        <div className={`${alertType} ${!subScriptionConfirm ?  "subscriptionAlert" : ""}`}>
-                            {subScriptionMessage}
+        <div className="container">
+
+       
+            <div className="row   align-items-center subscriptions">
+
+                <div className="col-md-6 offset-md-3" >
+                    <form className="  " onSubmit={subscriptionSubmit} >
+                        <div className=" input-group flex-nowrap mb-3">
+                            <div className="form-floating " >
+                                <div className={`${alertType} ${!subScriptionConfirm ?  "subscriptionAlert" : ""}`}>
+                                    {subScriptionMessage}
+                                </div>
+                                <input  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$" type="email" className={`form-control ${!subScriptionConfirm ?  "" : "subscriptionAlert"} subScriptionEmail`} id="floatingInputGrid " placeholder="name@example.com"   value={email} onChange={handleChange} required/>
+                                <label htmlFor="floatingInputGrid " className={`form-label ${!subScriptionConfirm ?  "" : "subscriptionAlert"}`}>Email address</label>
+                            </div>
+                            <button type="submit" className={`btn btn-dark btn-sm ${!subScriptionConfirm ?  "" : "subscriptionAlert"}`} >
+                                    <NewSubmitBTN 
+                                        size="35"
+                                        text="Subscribe"
+                                    />
+                            </button>
                         </div>
-                        <input  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$" type="email" className={`form-control ${!subScriptionConfirm ?  "" : "subscriptionAlert"} subScriptionEmail`} id="floatingInputGrid " placeholder="name@example.com"   value={email} onChange={handleChange} required/>
-                        <label htmlFor="floatingInputGrid " className={`form-label ${!subScriptionConfirm ?  "" : "subscriptionAlert"}`}>Email address</label>
-                    </div>
-                    <button type="submit" className={`btn btn-dark btn-lg ${!subScriptionConfirm ?  "" : "subscriptionAlert"}`} >
-                            <NewSubmitBTN 
-                                size="35"
-                                text="Subscribe"
-                            />
-                        </button>
+
+
+        
+
+
+                    </form>   
                 </div>
-
-                <div className="col-md-3 pt-4">
-                    <Social />
+                <div className="col-md-3 ">
+                        <Social />
                 </div>
- 
+                
+
+                
+            </div>     
+            
+            
+            
+            
+            
+            
+            
+        </div>
+                    
 
 
-            </form>            
 
 
-
-
-
-{/* Testing Section */}
-            <form className="row test justify-content-around g-3 mb-3"  >
-                <div className=" col">
-                    <div className="input-group">
-                        <div className="form-floating " >
-
-                            <input  type="email" className="form-control"  id="floatingInputGrid " placeholder="name@example.com" value={email} />
-                            <label htmlFor="floatingInputGrid ">Email address</label>  
-     
-                                                             
-                        </div>                    
-                        <button type="submit" className='btn btn-dark btn-sm testbtn '>
-                            <NewSubmitBTN 
-                                size="35"
-                                text="Subscribe"
-                            
-                            />
-                        
-                        </button> 
-                     
-                    </div>  
-                </div   >
-            </form>     
+    
 
          
 
-            {/* <div className="col-md-3 pt-4">
-                    <Social />
-                </div> */}
             
 
             
